c9131c00ce3960a841d9b81784f4d0c65df75475
Varun K <50563080+VarunK715@users.noreply.github.com>
2024-05-23 21:35:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 # MySQL configurations
 app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_USER'] = ''
 app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'todoapp'
 
